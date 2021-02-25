db9a9cf1e01f6247ff3de066de702a6f277d074c
MSebb23 <matteosabbatini23@gmail.com>
2021-02-25 19:55:59 +0100

    Completed p2
    A little bit more fancy

--- JS/script.js
+++ JS/script.js
@@ -2,6 +2,6 @@ var nome = prompt("Inserisci il tuo nome");
 var cognome = prompt("Inserisci il tuo cognome");
 var colore = prompt("Inserisci il tuo colore preferito");
 
-var password = "la tua password sarà : " + nome + cognome + colore + 27;
+var password = `La tua password sarà : ${nome}${cognome}${colore}${27}`;
 
 document.getElementById("anno_nascita").innerHTML = password;
